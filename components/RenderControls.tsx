2767a82902ed7577f51ec06b2ad6003722468eee
Suraj Pandey (Mac mini) <shpcore103@gmail.com>
2024-01-31 11:09:16 +0530

    Remove rendering button

--- components/RenderControls.tsx
+++ components/RenderControls.tsx
@@ -1,21 +1,22 @@
 import { z } from "zod"
 import { useRendering } from "../helpers/use-rendering"
 import { CompositionProps, COMP_NAME } from "../types/constants"
-import { AlignEnd } from "./AlignEnd"
-import { Button } from "./Button/Button"
+// import { AlignEnd } from "./AlignEnd"
+// import { Button } from "./Button/Button"
 import { InputContainer } from "./Container"
-import { DownloadButton } from "./DownloadButton"
-import { ErrorComp } from "./Error"
+// import { DownloadButton } from "./DownloadButton"
+// import { ErrorComp } from "./Error"
 import { Input } from "./Input"
-import { ProgressBar } from "./ProgressBar"
-import { Spacing } from "./Spacing"
+// import { ProgressBar } from "./ProgressBar"
+// import { Spacing } from "./Spacing"
 
 export const RenderControls: React.FC<{
   text: string
   setText: React.Dispatch<React.SetStateAction<string>>
   inputProps: z.infer<typeof CompositionProps>
 }> = ({ text, setText, inputProps }) => {
-  const { renderMedia, state, undo } = useRendering(COMP_NAME, inputProps)
+  // const { renderMedia, state, undo } = useRendering(COMP_NAME, inputProps)
+  const { state } = useRendering(COMP_NAME, inputProps)
 
   return (
     <InputContainer>
@@ -28,7 +29,7 @@ export const RenderControls: React.FC<{
             setText={setText}
             text={text}
           ></Input>
-          <Spacing></Spacing>
+          {/* <Spacing></Spacing>
           <AlignEnd>
             <Button
               disabled={state.status === "invoking"}
@@ -40,10 +41,10 @@ export const RenderControls: React.FC<{
           </AlignEnd>
           {state.status === "error" ? (
             <ErrorComp message={state.error.message}></ErrorComp>
-          ) : null}
+          ) : null} */}
         </>
       ) : null}
-      {state.status === "rendering" || state.status === "done" ? (
+      {/* {state.status === "rendering" || state.status === "done" ? (
         <>
           <ProgressBar
             progress={state.status === "rendering" ? state.progress : 1}
@@ -53,7 +54,7 @@ export const RenderControls: React.FC<{
             <DownloadButton undo={undo} state={state}></DownloadButton>
           </AlignEnd>
         </>
-      ) : null}
+      ) : null} */}
     </InputContainer>
   )
 }
